757b729d4df9f05f143a94de88493fd3b51138a9
ZhengXianda <xianda.zheng@jiamiantech.com>
2023-06-17 03:11:40 +0800

    补全引用，完善demo

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -3,11 +3,21 @@
 int main() {
   Debug = true;
 
+  // log
   Msg("Message.");
   Err("Error!");
 
-  SubscribeSignalCrash();
+  // iterator
+  vector<int> arr{0, 1, 2, 3, 4, 5};
+  for (int i = 0; i < arr.size(); i++) {
+    ForJump(int, v, arr, i) {
+      printf("%d - ", v);
+    }
+    printf("\n");
+  }
 
+  // signal
+  SubscribeSignalCrash();
   abort();
 
   return 1;
--- lib/include/tkcore/tkcore.hpp
+++ lib/include/tkcore/tkcore.hpp
@@ -4,6 +4,7 @@
 #include <tkcore/tk_base.hpp>
 #include <tkcore/tk_log.hpp>
 #include <tkcore/tk_signal.hpp>
+#include <tkcore/tk_iterator.hpp>
 
 using namespace tkht;
 
